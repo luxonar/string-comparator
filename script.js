4a43ff2e32c6e424e1d7d585b48d7f3c9ca75d9e
luxonar <l04konarik@email.cz>
2025-02-20 21:51:06 +0100

    updated script.js
    added new feature: params in url address

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+window.onload = function() {
+    getParams();
+    compare();
+};
+
+function getParams() {
+    const params = new URLSearchParams(window.location.search);
+    const string1 = params.get("first") || "";
+    const string2 = params.get("second") || "";
+    document.getElementById('string1').value = string1;
+    document.getElementById('string2').value = string2;
+}
+
 function compare() {
     let string1 = document.getElementById('string1').value;
     let string2 = document.getElementById('string2').value;
     let output = document.getElementById('output');
+
+    const params = new URLSearchParams(window.location.search);
+    params.set("first", string1);
+    params.set("second", string2);
+    window.history.replaceState({}, "", `${window.location.pathname}?${params}`);
 
     if (string1 == string2) {
         output.innerHTML = 'identical';
